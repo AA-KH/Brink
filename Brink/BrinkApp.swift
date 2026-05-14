dd66dad028cc3a29772a0022bc565ff51f8239aa
AA-KH <ak.luxa@gmail.com>
2026-05-15 03:41:55 +0530

    Initial Commit

---
+++ Brink/BrinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrinkApp.swift
+//  Brink
+//
+//  Created by Aaradhya Khanna on 15/05/26.
+//
+
+import SwiftUI
+
+@main
+struct BrinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
